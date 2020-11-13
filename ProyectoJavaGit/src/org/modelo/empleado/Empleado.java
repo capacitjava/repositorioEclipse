b80e72f7f0261dac3c77b82fda644256b75adfe8
Admin <Admin@DESKTOP-NQ8IN26>
2020-11-13 10:37:51 -0600

    //SE CREO EL MODELO EMPLEADO

---
+++ ProyectoJavaGit/src/org/modelo/empleado/Empleado.java
@@ -0,0 +1,29 @@
+package org.modelo.empleado;
+
+public class Empleado {
+
+	private int id;
+	private String nombre;
+	private double salario;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+}
